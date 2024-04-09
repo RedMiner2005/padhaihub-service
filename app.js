23170b469affa0f1b24bd07366bb296a4e60a864
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2024-04-10 01:06:13 +0530

    Initial Commit
    ... and hopefully everything is done

--- app.js
+++ app.js
@@ -2,60 +2,71 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const admin = require('firebase-admin');
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+
+initializeApp({
+    credential: applicationDefault(),
+});
+
+const db = admin.firestore();
+const usersDb = db.collection('users');
+const roomsDb = db.collection('rooms');
+
+app.get("/", (req, res) => res.type('json').send("Pratyush's PadhaiHub Backend Service"));
+app.get("/send/:toUserId/:roomId/:messageId", async (req, res) => {
+    const toUserId = req.params["toUserId"];
+    const roomId = req.params["roomId"];
+    const messageId = req.params["messageId"];
+
+    const toUserDoc = await usersDb.doc(toUserId).get(); 
+
+    if (!toUserDoc.exists) {
+        console.log('No user ' + toUserId);
+        res.send({"type": "error", "message": "InvalidToUserId"});
+        return;
+    } else {
+        console.log(toUserDoc.data());
+    }
+
+    const messageDoc = await roomsDb.doc(`${roomId}/messages/${messageId}`).get();
+
+    if (!messageDoc.exists) {
+        console.log('No message ' + messageId + ' or room ' + roomId);
+        res.send({"type": "error", "message": "InvalidMessageOrRoom"});
+        return;
+    } else {
+        console.log(messageDoc.data());
+    }
+
+    const fromUserDoc = await usersDb.doc(messageDoc.data()["authorId"]).get(); 
+
+    if (!fromUserDoc.exists) {
+        console.log('No (from) user ' + messageDoc.data()["authorId"]);
+        res.send({"type": "error", "message": "InvalidFromUserId"});
+        return;
+    }
+
+    const message = {
+        notification: {
+          title: 'New message',
+          body: 'From: ' + fromUserDoc.data()["firstName"],
+        },
+        token: toUserDoc.data()["notification_token"]
+      };
+      admin.messaging().send(message)
+        .then((resp) => {
+          console.log('Message sent successfully:', resp);
+          res.send({"type": "success", "message": "SentNotification"});
+        }).catch((err) => {
+          console.log('Failed to send the message:', err);
+          res.send({"type": "error", "message": "SendError"});
+        });
+})
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
